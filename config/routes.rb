361255cfd1c5a14b6d98c5e8e2e56269873f800e
Stella Mainar <mainars09@alumni.hanover.edu>
2019-09-07 10:34:37 -0600

    Add routes for welcome page, registration, and user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'welcome#index'
+
+  # Registration
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
+
+  # User Profile
+  get '/profile', to: 'users#show'
 end
