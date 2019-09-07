bd09a9cc07bec96f52edbd710cd6b2fc32c3a102
Sarah Tatro <statrox4@gmail.com>
2019-09-07 16:44:58 -0600

    Adjust service to dynamically take in filter attributes

--- app/services/service_service.rb
+++ app/services/service_service.rb
@@ -1,6 +1,13 @@
 class ServiceService
+
   def self.events_by_city(city)
     parse(fetch('/events', {city: city})).map! do |event_data|
+      Event.new(event_data)
+    end
+  end
+
+  def self.events_filtered(fields)
+    parse(fetch('/events', fields)).map! do |event_data|
       Event.new(event_data)
     end
   end
