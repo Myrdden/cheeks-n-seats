ac4df135bc75a6c52ec688142fc6986c8981d0b0
Stella Mainar <mainars09@alumni.hanover.edu>
2019-09-07 12:00:16 -0600

    Add role and active (boolean, default: true) to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates_presence_of :name
+  validates_presence_of :name, :role
   validates :email, presence: true, uniqueness: true
+
+  enum role: [:default, :admin]
 end
