f2dbfa031a06aedc39df0e3841faa9b05be48d0a
Sarah Tatro <statrox4@gmail.com>
2019-09-07 11:46:09 -0600

    Looked into routing issue with events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'entry#index'
-  get '/show', to: 'event#show'
   get '/index', to: 'event#index'
+  get '/show', to: 'event#show'
 end
