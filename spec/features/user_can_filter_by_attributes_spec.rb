624c984fe50db6ecd34d47e6258b89d29eda5191
Sarah Tatro <statrox4@gmail.com>
2019-09-07 11:45:18 -0600

    Create test for filter search functionality

---
+++ spec/features/user_can_filter_by_attributes_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature 'filter' do
+  scenario 'user can filter plays by price, genre, date' do
+    visit '/index'
+
+    expect(current_path).to eq('/index')
+    expect(page).to have_content('price')
+    expect(page).to have_content('genre')
+    expect(page).to have_content('date')
+  end
+end
