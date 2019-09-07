fff5662a28b6c1a5883d37131d3df1e0b7c3ae59
Sarah Tatro <statrox4@gmail.com>
2019-09-07 16:51:28 -0600

    Pass filter params into event controller

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -2,6 +2,16 @@ class EventController < ApplicationController
   def show
   end
   def index
-    @events = ServiceService.events_by_city('denver')    
+    filter_by = {}
+    if params[:genre]
+      filter_by[:genre] = params[:genre]
+    end
+    if params[:city]
+      filter_by[:city] = params[:city]
+    end
+    if params[:price]
+      filter_by[:price] = params[:price]
+    end
+    @events = ServiceService.events_filtered(filter_by)
   end
 end
