2a3bb637c5a27cc18508aaf633c4a77b2542cc4b
Sarah Tatro <statrox4@gmail.com>
2019-09-08 12:26:05 -0600

    javascript for date picker and range slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,10 +14,48 @@
 //= require activestorage
 //= require turbolinks
 //= require stimulus-init
+//= require materialize
+//= require nouislider
 //= require_tree .
 
 // M.AutoInit();
 $(document).ready(function(){
   $('select').formSelect();
-});
 
+  var sliderElem = document.getElementById('slider');
+  console.log(slider)
+  var slider = noUiSlider.create(sliderElem, {
+    start: [20, 80],
+    connect: true,
+    // step: 1,
+    // behaviour: 'unconstrained',
+    orientation: 'horizontal', // 'horizontal' or 'vertical'
+    range: {
+     'min': [0],
+     'max': [100]
+    }
+  });
+
+  var priceStart = document.getElementById('price-start');
+  var priceEnd = document.getElementById('price-end');
+  slider.on('update', function (values, handle) {
+      var value = values[handle];
+      if (handle == 1) {
+          priceEnd.value = Math.round(value);
+      } else {
+          priceStart.value = Math.round(value);
+      }
+  });
+
+
+  priceStart.addEventListener('change', function () {
+      slider.set([this.value, null]);
+  });
+
+  priceEnd.addEventListener('change', function () {
+      slider.set([null, this.value]);
+  });
+
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems);
+});
