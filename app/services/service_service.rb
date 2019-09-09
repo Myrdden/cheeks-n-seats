b4712ed37f19cf22ab09c9635fb4c8c867bbd7e8
Sarah Tatro <statrox4@gmail.com>
2019-09-09 16:57:21 -0600

    Consolidate or DRY up api calls

--- app/services/service_service.rb
+++ app/services/service_service.rb
@@ -1,22 +1,24 @@
 class ServiceService
 
-  def self.events_by_city(city)
-    parse(fetch('/events', {city: city})).map! do |event_data|
-      Event.new(event_data)
-    end
-  end
+  # def self.events_by_city(city)
+  #   event_data = parse(fetch('/events', {city: city}))
+  #   event_data.map do |event_data|
+  #     Event.new(event_data)
+  #   end
+  # end
 
   def self.events_filtered(fields)
-    parse(fetch('/events', fields)).map! do |event_data|
+    event_data = parse(fetch('/events', fields))
+    event_data.map do |event_data|
       Event.new(event_data)
     end
   end
 
-  def self.get_genres
-    parse(fetch('/genres', {})).map! do |genre_data|
-      GenreList.new(genre_data)
-    end
-  end
+  # def self.get_genres
+  #   parse(fetch('/genres', {})).map do |genre_data|
+  #     GenreList.new(genre_data)
+  #   end
+  # end
 
   private
   def self.fetch(uri, params)
