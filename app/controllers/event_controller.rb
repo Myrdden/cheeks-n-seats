c967226315d7ed35ccffbb784c0b5d6dfc5606ca
Sarah Tatro <statrox4@gmail.com>
2019-09-08 21:55:02 -0600

    Add params to filter by

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -9,9 +9,16 @@ class EventController < ApplicationController
     if params[:city]
       filter_by[:city] = params[:city]
     end
-    if params[:price]
-      filter_by[:price] = params[:price]
+    if params[:price_start]
+      filter_by[:price_start] = params[:price_start]
+    end
+    if params[:price_end]
+      filter_by[:price_end] = params[:price_end]
+    end
+    if params[:date]
+      params[:date]
     end
     @events = ServiceService.events_filtered(filter_by)
+    @genres = ServiceService.get_genres()
   end
 end
