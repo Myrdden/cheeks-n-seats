eab83e7b0603b16f3df26637f7f936c058d715bc
Sarah Tatro <statrox4@gmail.com>
2019-09-09 20:52:15 -0600

    Get rid of unnecessary code

--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -1,21 +1,8 @@
 class EventService
 
-  # def self.events_by_city(city)
-  #   event_data = parse(fetch('/events', {city: city}))
-  #   event_data.map do |event_data|
-  #     Event.new(event_data)
-  #   end
-  # end
-
   def fetch_events(fields)
     parse(fetch('/events', fields))
   end
-
-  # def self.get_genres
-  #   parse(fetch('/genres', {})).map do |genre_data|
-  #     GenreList.new(genre_data)
-  #   end
-  # end
 
   private
   def fetch(uri, params)
