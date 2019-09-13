2524be9947514424fe6b84d2c5b98f69ee82c1b4
Aurie <Myrdden42@gmail.com>
2019-09-12 21:00:44 -0600

    Added ENV Vars

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,12 @@ module CheeksNSeats
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
