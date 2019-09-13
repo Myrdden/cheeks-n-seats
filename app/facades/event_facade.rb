91a31cdef3b75d13fc914259658f8f55abb05532
Aurie <Myrdden42@gmail.com>
2019-09-12 22:26:51 -0600

    Add Filter Method

--- app/facades/event_facade.rb
+++ app/facades/event_facade.rb
@@ -31,6 +31,7 @@ class EventFacade
       end
     end
     response = response.values
+    binding.pry
     response = filter(response, @fields) if !@fields.empty?
     redis.set_by(@fields, response.to_json) if update
     return response
@@ -48,6 +49,30 @@ class EventFacade
   private
 
   def filter(events, params)
-    
+    events.keep_if do |event|
+      valid = true
+      params.each do |k,v|
+        case k
+        when 'minPrice'
+          valid = false if event.min_price >= v.to_i
+        when 'maxPrice'
+          valid = false if event.max_price <= v.to_i
+        when 'date'
+          valid = false if !event.dates.any? {|date| match_date(date[:date], v)}
+        end
+      end
+      valid
+    end
+  end
+
+  def match_date(event, param)
+    month_nums = {'Jan' => 1, 'Feb' => 2, 'Mar' => 3, 'Apr' => 4,
+                  'May' => 5, 'Jun' => 6, 'Jul' => 7, 'Aug' => 8,
+                  'Sep' => 9, 'Oct' => 10, 'Nov' => 11, 'Dec' => 12}
+    event_date = event.split('-')
+    param_date = param.split(' ')
+    param_date[0] = month_nums[param_date[0]]
+    param_date[1] = param_date[1].delete(',')
+    event_date[0] == param_date[2] && event_date[1] == param_date[0] && event_date[2] == param_date[1]
   end
 end
