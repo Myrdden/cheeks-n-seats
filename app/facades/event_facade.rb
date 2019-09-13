e7f18b1abd10ae71355f9c97d4fb6cbfe3b00e2c
Aurie <Myrdden42@gmail.com>
2019-09-12 20:04:50 -0600

    Add Caching on Facade

--- app/facades/event_facade.rb
+++ app/facades/event_facade.rb
@@ -5,19 +5,35 @@ class EventFacade
     @priceMin = fields[:minPrice]
     @priceMax = fields[:maxPrice]
     @date = fields[:date]
-    @fields = fields
+    unneeded = ['utf8', 'controller', 'action', 'commit']
+    @fields = {}
+    fields.reject! {|key| unneeded.include?(key)}.each do |k, v|
+      @fields[k] = v
+    end
   end
 
   def events
     response = {}
-    EventService.fetch_events(@fields).each do |event_data|
+    update = false
+    redis = RedisService.new
+    events = redis.get_by(@fields)
+    if !events
+      events = EventService.fetch_events
+      update = true
+    else
+      events = JSON.parse(events, symbolize_names: true)
+    end
+    events.each do |event_data|
       if response[event_data[:name]]
         response[event_data[:name]].add_date(event_data[:date], event_data[:time], event_data[:url])
       else
         response[event_data[:name]] = Event.new(event_data)
       end
     end
-    response.values
+    response = response.values
+    #response = filter(response, @fields) if !@fields.empty?
+    redis.set_by(@fields, response.to_json) if update
+    return response
   end
 
   def genres
@@ -31,5 +47,7 @@ class EventFacade
 
   private
 
-  attr_reader :service
+  def filter(events, params)
+    
+  end
 end
