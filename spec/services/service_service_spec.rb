21f81c5637df7179f4c1fdf6f6ed10ffbf1bab99
Aurie <Myrdden42@gmail.com>
2019-09-12 18:23:32 -0600

    Removed Outdated Test

--- spec/services/service_service_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe ServiceService do
-  it 'can query for events by city' do
-    event_data = ServiceService.events_filtered({city: "Denver"})
-
-    expect(event_data).to be_an(Array)
-  end
-end
